4fec25e385485d30d8a136bed4b99a583f30904e
Nikola Aleksic <aleksicnikola46@gmail.com>
2021-08-25 14:17:20 +0200

    Initialize commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//Components
+import Header from './components/Header';
+import Todo from './components/Todo/Todo'
+
+//Styles
+import {GlobalStyle} from './GlobalStyle';
+import {TodoStyles} from './components/Todo/TodoStyles';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <h1>TODOS</h1>
+            <Todo text='Learn React' />
+            <GlobalStyle/>
+        </div>
+    );
 }
 
 export default App;
+
+// const Start = () => React.createElement('div', null, 'This is a element');
+// const App = () => {
+//   return Start();
+// }
+// function App() {
+//     return (
+//         <div className="App">
+//             Start here.
+//         </div>
+//     );
+// }
+
---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Todo = () => {
+    return (
+        <div>
+            <h1>My Todos</h1>
+            <div>
+                <h2>TITLE</h2>
+                <div>
+                    <button>Delete</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Todo
